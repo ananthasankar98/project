fe88027791c2b8dc94c7fda958719f2b6c2816de
ananthasankar98 <93582748+ananthasankar98@users.noreply.github.com>
2022-03-07 21:27:40 +0530

    prjt

--- src/components/eventstable/EventsTable.js
+++ src/components/eventstable/EventsTable.js
@@ -1,130 +1,63 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function EventsTable(props) {
     return (
-        <div>            
-            <div class="flex flex-col">
-            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-md sm:rounded-lg">
-            <table class="min-w-full">
-            <thead class="bg-gray-50 dark:bg-gray-700">
-            <tr>
-            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-            Events
-            </th>
-            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-            Fees
-            </th>
-            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-            Starting Date
-            </th>
-            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-            Duration
-            </th>
-            <th scope="col" class="relative py-3 px-6">
-            <span class="sr-only">Edit</span>
-            </th>
-            </tr>
-            </thead>
-            <tbody>
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            Digital Teaching Techniques
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            750 INR +GST
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            16th June
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                        5 days
-            </td>
-            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-            <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            </td>
-            </tr>
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            Internship 2022
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            1990 INR +GST
-            </td>
-                        <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            10th Sept
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            14 Days
-            </td>
-            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-            <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            </td>
-            </tr>
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            Salesforce
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            6300 INR +GST
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            5th Oct
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            45 Days
-            </td>
-            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-            <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            </td>
-            </tr>
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            ICTSET
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            INR +GST
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            2022
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            Days
-            </td>
-            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-            <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            </td>
-            </tr>
-
-            <tr class="bg-white dark:bg-gray-800">
-            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            Techathlon
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            INR +GST
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            2022
-            </td>
-            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-            Days
-            </td>
-            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-            <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            </td>
-            </tr>
-            </tbody>
-            </table>
+        <div> 
+            <br></br> 
+            <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ml-6">1. ICSET 2022</h1>
+            <br></br>
+            <a href="#" class="block p-6 pr-500 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ml-6">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ICSET 2022</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400 ">Lorem ipsum dolor sit amet. Nam aspernatur iste 33 aliquam quos eum fugiat sint est quia minus. Aut necessitatibus blanditiis aut odio corrupti et consectetur consequuntur in ducimus quibusdam est quos debitis sed vero amet ea dolore ipsa. Ut nesciunt itaque sed earum veniam sit eveniet ipsa quo fuga vero in saepe recusandae qui facere omnis a odit voluptas. Sit quia dolores est omnis Quis rem provident suscipit sed dolorem incidunt qui iusto cupiditate ea quam dolorem sit veritatis consequuntur.</p>
+            </a>
+        
+            <div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ml-6 mt-6 pt-5">
+            <a href="#">
+            </a>
+            <div class="px-5 pb-5">
+            <a href="#">
+            <h5 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">Registration details and pricing</h5>
+            </a> <br></br>
+            <p class="text-xl font-semibold text-gray-900 dark:text-white">Registration opens on: 3rd Apr 2022</p>
+            <p class="text-xl font-semibold text-gray-900 dark:text-white">Start Date: 03-8-2022</p>
+            <p class="text-xl font-semibold text-gray-900 dark:text-white">End Date:03-12-2022</p>    
+            <br></br>
+            <div class="flex justify-between items-center">
+            <span class="text-3xl font-semibold text-gray-900 dark:text-white">950 INR+GST</span>
+            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</a>
             </div>
+            </div>
+            </div>
+
+            <br></br> 
+            <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ml-6">2. Techathlon</h1>
+            <br></br>
+            <a href="#" class="block p-6 pr-500 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ml-6">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Techathlon 2022</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400 ">Lorem ipsum dolor sit amet. Nam aspernatur iste 33 aliquam quos eum fugiat sint est quia minus. Aut necessitatibus blanditiis aut odio corrupti et consectetur consequuntur in ducimus quibusdam est quos debitis sed vero amet ea dolore ipsa. Ut nesciunt itaque sed earum veniam sit eveniet ipsa quo fuga vero in saepe recusandae qui facere omnis a odit voluptas. Sit quia dolores est omnis Quis rem provident suscipit sed dolorem incidunt qui iusto cupiditate ea quam dolorem sit veritatis consequuntur.</p>
+            </a>
+        
+            <div class="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ml-6 mt-6 pt-5">
+            <a href="#">
+            </a>
+            <div class="px-5 pb-5">
+            <a href="#">
+            <h5 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">Registration details and pricing</h5>
+            </a> <br></br>
+            <p class="text-xl font-semibold text-gray-900 dark:text-white">Registration opens on: 10th Aug 2022</p>
+            <p class="text-xl font-semibold text-gray-900 dark:text-white">Start Date: 05-10-2022</p>
+            <p class="text-xl font-semibold text-gray-900 dark:text-white">End Date:05-12-2022</p>    
+            <br></br>
+            <div class="flex justify-between items-center">
+            <span class="text-3xl font-semibold text-gray-900 dark:text-white">950 INR+GST</span>
+            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</a>
             </div>
             </div>
             </div>
         </div>
+        
     );
 }
 
